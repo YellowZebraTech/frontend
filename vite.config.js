5227357de5b2a459fa83377172c2c36fd9f386c0
NS <engineer.siu@gmail.com>
2025-07-23 11:00:09 -0300

    vite base change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/sortingfrontend/",
+  base: "/frontend/",
 
   plugins: [react(), tailwindcss()],
   server: { //    
