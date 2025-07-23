629625dab5e978607d942beb5bcb0a5d5f8fa3d1
NS <engineer.siu@gmail.com>
2025-07-23 11:04:30 -0300

    few chages in vite

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/frontend/",
+  base: "/sorting/",
 
   plugins: [react(), tailwindcss()],
-  server: { //    
-    host: '0.0.0.0', //
-    port: 5173,//
+  //server: { //    
+  //  host: '0.0.0.0', //
+  //  port: 5173,//
     //strictPort: true // Evita cambio automático de puerto
-  }
+ // }
 })
